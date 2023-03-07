d52f37c20d12a33c19afdc32b65423876406e1fc
ccaparas666 <carmelo.caparas@lccdo.edu.ph>
2023-03-07 18:30:04 +0800

    Navigation IONIC

--- src/app/page1/page1.page.ts
+++ src/app/page1/page1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {NavController,AlertController} from '@ionic/angular';
+
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.page.html',
@@ -7,15 +8,34 @@ import {NavController} from '@ionic/angular';
 })
 export class Page1Page implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private alertController: AlertController) { }
   userinput1: any = '';
   userinput2: any = '';
+
+  checkInput(){
+    const userinput1 = document.getElementById('user-input1')as HTMLIonTextareaElement;
+    const userinput2 = document.getElementById('user-input2')as HTMLIonTextareaElement;
+
+    if(!userinput1.value || !userinput2.value){
+       const alert = this.alertController.create({message: 'Please enter an Input. Thanks.',
+       buttons:['OK']});
+       alert.then((alert)=> alert.present());
+    } else{
+      this.GotoOutputPage()
+    }
+  }
   GotoOutputPage(){
     const userinput1 = (document.getElementById('user-input1')as HTMLInputElement).value;
+    const value1 = Number(userinput1);
 
     const userinput2 = (document.getElementById('user-input2')as HTMLInputElement).value;
+    const value2 = Number(userinput2);
 
-    this.navCtrl.navigateForward(['/page2',{userinput1, userinput2}]);
+    const Valsum = value1 + value2;
+
+    this.navCtrl.navigateForward(['/page2',{userinput1, userinput2, Valsum}]);
+
+    
   }
 
   ngOnInit() {
--- src/app/page2/page2.page.ts
+++ src/app/page2/page2.page.ts
@@ -10,12 +10,30 @@ import { ActivatedRoute } from '@angular/router';
 export class Page2Page implements OnInit {
 
   constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute) {  }
-   userinput1: any = 'userinput1';
-   userinput2: any = 'userinput12';
+   userinput1: any = 'user-input1';
+   userinput2: any = 'user-input2';
+   Valsum: any = 'Valsum';
+
+   GotoInputPage(){
+    this.navCtrl.navigateBack(['/page1']);
+    this.clearTextArea();
+   }
+   clearTextArea(){
+    const textarea1 = document.querySelector(
+      '#user-input1'
+    ) as HTMLIonInputElement;
+    textarea1.value='';
+
+    const textarea2 = document.querySelector(
+      '#user-input2'
+    ) as HTMLIonInputElement;
+    textarea2.value='';
+  }
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
       this.userinput1 = params['userinput1'];
       this.userinput2 = params['userinput2'];
+      this.Valsum = params['Valsum'];
 
     });
   }
